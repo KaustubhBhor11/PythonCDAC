48c42050d692b807a259881a3a7327b5cf3a9836
KaustubhBhor11 <kaustubh.bhor39@gmail.com>
2025-03-18 10:47:47 +0530

    Function for string operation

--- functions.py
+++ functions.py
@@ -2,10 +2,6 @@
 def hello() -> None:
     print("hello world")
 
-
-hello()
-
-
 # Factorial of a number
 def factorial(n) -> int:
     fact = 1
@@ -14,18 +10,11 @@ def factorial(n) -> int:
     return fact
 
 
-print(factorial(5))
-
-
 def is_prime(n) -> bool:
     for i in range(2, n):
         if n % i == 0:
             return False
     return True
-
-
-print(is_prime(10))
-
 
 def is_odd_even(n) -> None:
     if n % 2 == 0:
@@ -33,20 +22,12 @@ def is_odd_even(n) -> None:
     else:
         print('Number is odd')
 
-
-is_odd_even(12)
-
-
 # Variable argument function - similar to ... in Java
 def add(*a) -> int:
     ans = 0
     for i in a:
         ans += i
     return ans
-
-
-print(add(1, 5, 2, 7, 1, 8, 2, 6, 1, 9))
-
 
 # Lambda/Anonymous function
 def add(a, b) -> int:
@@ -76,4 +57,82 @@ def convert_identifier_case(s) -> str:
             else:
                 s1 += s[i].lower()
         return s1
+
+# //prime numbers for range 10-20 30-40 in 1 list
+def is_prime_odd():
+    prime_list = []
+    for i in range(2, 41):
+        # print(i)
+        if (9 < i and i < 21) or (29 < i and i < 41):
+            if is_prime(i):
+                prime_list.append(i)
+    return prime_list
+
+
+# recursion to find factorial
+def fact(n):
+    if n <= 1:
+        return 1
+    else:
+        return n * fact(n - 1)
+
+
+# List comprehension
+x = [i for i in range(0, 11) if is_prime(i)]
+
+list_of_input = [10, 12, 68, 3, 2, 7, 9, 11, 56, 43, 67]
+even = [i for i in list_of_input if (i % 2 == 0)]
+odd = [i for i in list_of_input if (i % 2 != 0)]
+prime = [i for i in list_of_input if is_prime(i)]
+
+list_of_names = ["ajay", "pratik", "asam"]
+t = [i for i in list_of_names if i[0] == "a"]
+
+# number of vovels should me more then one
+vowel = ["a", "e", "i", "o"]
+
+
+# make list of strings that have 1 or more vowels
+def count_vowels(s):
+    count = 0
+    for i in s:
+        if i in vowel:
+            count += 1
+    return count
+
+
+c = [i for i in list_of_names if count_vowels(i.lower()) > 2]
+
+# assignment
+# make list of strings that have 1 or more consonents
+input_String = "this is fun"
+s = ""
+
+
+def encode_string(s: str) -> str:
+    final_string = ""
+    for i in s:
+        if (i not in vowel) and (i != " "):
+            final_string += i + "o" + i
+        else:
+            final_string += i
+    return final_string
+
+
+# Assignment 2
+def string_greater_than_n(l: list, n: int) -> list:
+    result = [i for i in l if len(i) > n]
+    return result
+
+
+# iterator
+iterator = iter(list_of_input)
+print(next(iterator))
+
+
+print(list_of_input)
+print(sorted(list_of_input))
+print(list_of_input)
+print([x for x in reversed(list_of_input)])
+print(list_of_input)
 
